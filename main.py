7c0662229f14312760461c70d57d85a1b61e917e
Anton Tovshchyk <totonn777@gmail.com>
2023-04-24 22:11:55 -0500

    Refactor

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/create_database')
+def create_database():
+    conn = sqlite3.connect('flask/api/mydatabase.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE users
+                 (ID INT PRIMARY KEY     NOT NULL,
+                 userName           TEXT    NOT NULL,
+                 Password            TEXT     NOT NULL);''')
+    conn.commit()
+    conn.close()
+    return 'Database created successfully!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+    def index():
+        conn = sqlite3.connect('flask/api/mydatabase.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM users')
+        rows = c.fetchall()
+        conn.close()
+        return render_template('index.html', rows=rows)
+
+
+    if __name__ == '__main__':
+        app.run()
